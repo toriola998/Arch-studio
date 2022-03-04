a4c391d77e1ccd225fecb32dc64104817f0fa9b3
Toriola Faidat <faaedah@gmail.com>
2022-03-04 20:06:04 +0100

    created about view

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+    <Header/>
+    <Footer />
+</template>
+
+<script>
+import Header from './../components/Header.vue'
+import Footer from './../components/Footer.vue'
+export default {
+    name: 'About',
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+
+</style>>

11962ae69eab3caf477acbddfad8fbd8a84cec7a
Toriola Faidat <faaedah@gmail.com>
2022-03-06 19:49:42 +0100

    created ContactInfo component

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="contact-info-wrap">
+        <p class="heading-p">{{ headingTitle }}</p>
+        <div class="contact-info">
+            <a href=""> {{ mail }}</a>
+            <address> {{ address }}</address>
+            <a href=""> {{ phone }}</a>
+        </div>
+        <div class="flex">
+            <p><strong>View on Map</strong></p>
+            <img src="./../assets/icons/icon-arrow.svg" alt="go-to-arrow"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactDetails',
+    props: ['headingTitle', 'mail', 'address', 'phone']
+}
+</script>
+
+<style scoped>
+    .contact-info {
+        margin: 1rem 0;
+    }
+
+    .contact-info a,
+    .contact-info address{
+        font-weight: 500;
+        font-size: .9rem;
+        font-style: normal;
+        padding-bottom: .8rem;
+        display: block;
+        color: #60636D;
+    }
+
+    .heading-p {
+        font-weight: 700;
+    } 
+
+    .contact-info-wrap {
+        margin-top: 3.5rem ;
+    }
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,35 @@
 <template>
-  
+    <Header />
+    <section class="contact">
+        <div class="line"></div>
+        <h2>Contact<br> Details</h2>
+        <ContactInfo headingTitle="Main Office" mail="Mail: archone@mail.com"
+                    address="Address: 1892  Chenoweth Drive TN" phone="Phone: 123-456-3451" />
+
+        <ContactInfo headingTitle="Office II" mail="Mail: archtwo@mail.com"
+                    address="Address: 3399  Wines Lane TX" phone="Phone: 832-123-4321" />
+    </section>
+    <Footer />
 </template>
 
 <script>
+import Header from './../components/Header.vue'
+import ContactInfo from './../components/ContactInfo.vue'
+import Footer from './../components/Footer.vue'
 export default {
-
+    name: 'Contact',
+    components: {
+        Header,
+        ContactInfo,
+        Footer
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    .contact {
+        padding: 0 1.5rem 4rem;
+    }
 
+    
 </style>

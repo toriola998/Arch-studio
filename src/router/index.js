7ecb2025bff4a17449c1edf30269a24d122b58f1
Toriola Faidat <faaedah@gmail.com>
2022-03-06 18:43:21 +0100

    created route/view for contact page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory} from 'vue-router';
 import Home from './../views/Home.vue'
 import Portfolio from './../views/Portfolio.vue'
 import About from './../views/About.vue'
+import Contact from './../views/Contact.vue'
 
 const router = createRouter({
     history:  createWebHistory(),
@@ -23,6 +24,12 @@ const router = createRouter({
         path: '/about-us',
         name: 'about',
         component: About
+    },
+
+    {
+        path: '/contact',
+        name: 'contact',
+        component: Contact
     },
 
     ]
